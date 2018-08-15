2bbde223e81100f2a20a2c62013ae7ae3a53244c
Özcan ÇAMOĞLU <ozcan.camoglu@bil.omu.edu.tr>
2018-08-15 11:46:26 +0300

    file library

---
+++ classFile.php
@@ -0,0 +1,42 @@
+<?php
+
+interface FileInterface {
+    public function get();
+	public function set();
+	public function getSize();
+	public function setSize();
+	public function getName();
+	public function setName();
+	public function getType();
+	public function setType();
+	public function setError();
+	public function getError();
+	public function getTmpName();
+	public function setTmpName();
+	public function upload();
+	public function delete();
+	public function status();
+	public function copy();
+	public function getPath();
+	public function setPath();
+}
+
+interface ImgInterface extends FileInterface () {
+	public function getWidth();
+	public function setWidth();
+	public function getHeight();
+	public function setHeight();
+	public function getOpacity(); 
+	public function setOpacity();
+	public function getColor();
+	public function setColor();
+	public function rotate();
+
+
+class File implements FileInterface {
+
+}
+
+class Img extends File implements ImgInterface {
+
+}
